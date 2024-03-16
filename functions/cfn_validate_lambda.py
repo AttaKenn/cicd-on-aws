268ef1e867a136f08ac7dce23b12be35380347e4
AttaKenn <kennethattaoppong@gmail.com>
2024-03-16 23:22:10 +0000

    CFN Validate Lambda: added function find_artifact

--- functions/cfn_validate_lambda.py
+++ functions/cfn_validate_lambda.py
@@ -13,3 +13,22 @@ import zipfile
 import time
 
 print('Loading function')
+
+cf = boto3.client('cloudformation')
+code_pipeline = boto3.client('codepipeline')
+
+def find_artifact(artifacts, name):
+    """Finds the artifact 'name' among the 'artifacts'
+
+    Args:
+        artifacts: The list of artifacts available to the function
+        name: The artifact to be used
+    Returns:
+        The artifact dictionary found
+
+    """
+    for artifact in artifacts:
+        if artifact['name'] == name:
+            return artifact
+
+    raise Exception('Input artifact named "{0}" not found in event'.format(name))
