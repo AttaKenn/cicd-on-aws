7a9f752d019111aff13d3652e5e7d89a30558515
AttaKenn <kennethattaoppong@gmail.com>
2024-03-17 14:14:17 +0000

    CFN Validate Lambda: added function get_user_params

--- functions/cfn_validate_lambda.py
+++ functions/cfn_validate_lambda.py
@@ -23,6 +23,7 @@ def find_artifact(artifacts, name):
     Args:
         artifacts: The list of artifacts available to the function
         name: The artifact to be used
+
     Returns:
         The artifact dictionary found
 
@@ -102,3 +103,44 @@ def continue_job_later(job, message):
     print('Putting job continuation')
     print(message)
     code_pipeline.put_job_success_result(jobId=job, continuationToken=continuation_token)
+
+def get_user_params(job_data):
+    print(job_data)
+    """Decodes the JSON user parameters and validates the required properties.
+
+    Args:
+        job_data: The job data structure containing the UserParameters string which should be a valid JSON structure
+
+    Returns:
+        The JSON parameters decoded as a dictionary.
+
+    Raises:
+        Exception: The JSON can't be decoded or a property is missing.
+
+    """
+    try:
+        # Get the user parameters which contain the artifact and file settings
+        user_parameters = job_data['actionConfiguration']['configuration']['UserParameters']
+        decoded_parameters = json.loads(user_parameters)
+
+    except Exception as e:
+        # Expecting the user parameters to be JSON encoded in order to pass multiple values.
+        # If the JSON can't be decoded the job is failed with a helpful message.
+        raise Exception('UserParameters could not be decoded as JSON')
+
+    if 'input' not in decoded_parameters:
+        # Validate that the artifact name is provided, otherwise the job is failed
+        # with a helpful message.
+        raise Exception('Your UserParameters JSON must include the artifact name')
+
+    if 'file' not in decoded_parameters:
+        # Validate that the template file is provided, otherwise the job is failed
+        # with a helpful message.
+        raise Exception('Your UserParameters JSON must include the template file name')
+
+    if 'output' not in decoded_parameters:
+        # Validate that the output bucket is provided, otherwise the job is failed
+        # with a helpful message.
+        raise Exception('Your UserParameters JSON must include the output bucket')    
+
+    return decoded_parameters
