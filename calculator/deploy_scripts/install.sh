2517d9248b84d18a2bc9dc29102ba70398a086fd
AttaKenn <kennethattaoppong@gmail.com>
2024-03-19 16:26:28 +0000

    Deploy Scripts: install.sh modified

--- calculator/deploy_scripts/install.sh
+++ calculator/deploy_scripts/install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user
-node -v
-if [ $? -ne 0 ]
+
+# Check Node Version
+if [ ! -x "$(command -v node)" ]  # Check if node executable exists
 then
-   curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
-   . ~/.nvm/nvm.sh
-   nvm install 4.3.2
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+  . ~/.nvm/nvm.sh
+  nvm install lts/*
 fi
